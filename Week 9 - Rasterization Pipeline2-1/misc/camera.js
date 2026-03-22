32aedf5ab0a71219daef9e991971a24d2d4730d7
lemendez-hub <lemendez@csumb.edu>
2026-03-21 21:41:09 -0700

    TODO 7 and 8
    Completed 7 and 8.
    Fixed an error on todo 6.

--- Week 9 - Rasterization Pipeline2-1/misc/camera.js
+++ Week 9 - Rasterization Pipeline2-1/misc/camera.js
@@ -23,7 +23,7 @@ class Camera {
     // todo #6 - pull out the forward direction from the world matrix and return as a vector
     //         - recall that the camera looks in the "backwards" direction
     var camE = this.cameraWorldMatrix.elements;
-    return new Vector3(-camE[8], -camE[9], -camE[10]).normalize;
+    return new Vector3(-camE[8], -camE[9], -camE[10]).normalize();
   }
 
   // -------------------------------------------------------------------------
@@ -32,23 +32,29 @@ class Camera {
 
     if (this.input.up) {
       // todo #7 - move the camera position a little bit in its forward direction
+      this.cameraPosition.add(currentForward.clone().multiplyScalar(5 * dt));
     }
 
     if (this.input.down) {
       // todo #7 - move the camera position a little bit in its backward direction
+      this.cameraPosition.subtract(currentForward.clone().multiplyScalar(5 * dt));
     }
 
     if (this.input.left) {
       // todo #8 - add a little bit to the current camera yaw
+      this.cameraYaw += 90 * dt;
     }
 
     if (this.input.right) {
       // todo #8 - subtract a little bit from the current camera yaw
+      this.cameraYaw -= 90 * dt;
     }
 
     // todo #7 - create the cameraWorldMatrix from scratch based on this.cameraPosition
+    this.cameraWorldMatrix.makeTranslation(this.cameraPosition);
 
     // todo #8 - create a rotation matrix based on cameraYaw and apply it to the cameraWorldMatrix
     // (order matters!)
+    this.cameraWorldMatrix.multiply(new Matrix4().makeRotationY(this.cameraYaw));
   }
 }
