9c0e213da55bf601aa8b4abfad8448c5b7ac5bb5
lemendez-hub <lemendez@csumb.edu>
2026-04-16 19:12:32 -0700

    Week12 TODO 2
    completed Todo 2

--- Week 12 - Phong Directional/shaders/phong.pointlit.fs.glsl
+++ Week 12 - Phong Directional/shaders/phong.pointlit.fs.glsl
@@ -9,35 +9,34 @@ varying vec3 vWorldNormal;
 varying vec3 vWorldPosition;
 
 void main(void) {
-    // // diffuse contribution
-    // // todo #1 normalize the light direction and store in a separate variable
-    // vec3 normalizedLD = normalize(uLightPosition);
-    // // todo #2 normalize the world normal and store in a separate variable
-    // vec3 normalizedWN = normalize(vWorldNormal);
-    // // todo #3 calculate the lambert term
-    // float lambert = max(dot(normalizedWN, normalizedLD), 0.0);
+    // diffuse contribution
+    // todo #1 normalize the light direction and store in a separate variable
+    vec3 normalizedLP = normalize(uLightPosition - vWorldPosition);
+    // todo #2 normalize the world normal and store in a separate variable
+    vec3 normalizedWN = normalize(vWorldNormal);
+    // todo #3 calculate the lambert term
+    float lambert = max(dot(normalizedWN, normalizedLP), 0.0);
 
-    // // specular contribution
-    // // todo #4 in world space, calculate the direction from the surface point to the eye (normalized)
-    // vec3 direction = normalize(uCameraPosition - vWorldPosition);
-    // // todo #5 in world space, calculate the reflection vector (normalized)
-    // vec3 reflection = normalize(reflect(-normalizedLD, normalizedWN));
-    // // todo #6 calculate the phong term
-    // float phong = pow(max(dot(direction, reflection), 0.0), 64.0);
-    // // combine
-    // // todo #7 apply light and material interaction for diffuse value by using the texture color as the material
-    // // todo #8 apply light and material interaction for phong, assume phong material color is (0.3, 0.3, 0.3)
+    // specular contribution
+    // todo #4 in world space, calculate the direction from the surface point to the eye (normalized)
+    vec3 direction = normalize(uCameraPosition - vWorldPosition);
+    // todo #5 in world space, calculate the reflection vector (normalized)
+    vec3 reflection = normalize(reflect(-normalizedLP, normalizedWN));
+    // todo #6 calculate the phong term
+    float phong = pow(max(dot(direction, reflection), 0.0), 64.0);
+    // combine
+    // todo #7 apply light and material interaction for diffuse value by using the texture color as the material
+    // todo #8 apply light and material interaction for phong, assume phong material color is (0.3, 0.3, 0.3)
 
-    // vec3 albedo = texture2D(uTexture, vTexcoords).rgb;
+    vec3 albedo = texture2D(uTexture, vTexcoords).rgb;
 
-    // vec3 ambient = albedo * 0.1;
-    // vec3 diffuseColor = albedo * lambert;
-    // vec3 specularColor = vec3(0.3, 0.3, 0.3) * phong;
+    vec3 ambient = albedo * 0.1;
+    vec3 diffuseColor = albedo * lambert;
+    vec3 specularColor = vec3(0.3, 0.3, 0.3) * phong;
 
-    // // todo #9
-    // // add "diffuseColor" and "specularColor" when ready
-    // vec3 finalColor = ambient + diffuseColor + specularColor;
+    // todo #9
+    // add "diffuseColor" and "specularColor" when ready
+    vec3 finalColor = ambient + diffuseColor + specularColor;
 
-    // gl_FragColor = vec4(finalColor, 1.0);
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    gl_FragColor = vec4(finalColor, 1.0);
 }
