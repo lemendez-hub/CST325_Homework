ae07e7af9cc7bb0556b5ed0a3d950d51b1024df8
lemendez-hub <lemendez@csumb.edu>
2026-04-22 17:21:33 -0700

    todo 5 and 6
    Completed todo 5 and 6

--- Week 13 - Shadow Mapping-1/shaders/phong.fs.glsl
+++ Week 13 - Shadow Mapping-1/shaders/phong.fs.glsl
@@ -29,10 +29,10 @@ void main(void) {
 
     // todo #6
     // transform the world position into the lights clip space (clip space and NDC will be the same for orthographic projection)
-    // vec4 lightSpaceNDC = ...;
+    vec4 lightSpaceNDC = uLightVPMatrix * vec4(vWorldPosition, 1.0);
 
     // scale and bias the light-space NDC xy coordinates from [-1, 1] to [0, 1]
-    // vec2 lightSpaceUV = ...;
+    vec2 lightSpaceUV = lightSpaceNDC.xy * 0.5 + 0.5;
 
     // todo #7
     // Sample from the shadow map texture using the previously calculated lightSpaceUV
@@ -45,11 +45,15 @@ void main(void) {
     float bias = 0.004;
 
     // todo #9
-    gl_FragColor = vec4(finalColor, 1.0); // remove this when you are ready to add shadows
+    // gl_FragColor = vec4(finalColor, 1.0); // remove this when you are ready to add shadows
     //if (/* in shadow*/) {
     //   gl_FragColor = vec4(ambient, 1.0);
     //} else {
     //    gl_FragColor = vec4(finalColor, 1.0);
     //}
+
+    vec4 shadowColor = texture2D(uShadowTexture, vTexCoords);
+    gl_FragColor = vec4(lightSpaceUV.x, lightSpaceUV.y, 0.0, 1.0);
+
 }
 
